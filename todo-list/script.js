9032b00a63140bbb443500c4749e80a0c77569a8
saurav-verma-01 <sauravkumarverma22@gmail.com>
2024-05-11 20:28:18 +0530

    revised project with clean and efficient code

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,14 +1,27 @@
-const todoList = document.querySelector(".todo-list");
+// 1. Select all Elements
+const form = document.querySelector(".form");
+const todoInput = document.querySelector("#todo");
+const list = document.querySelector(".todo-list");
+// 2. Add Event listener to the form Submission
 
-const itemForm = document.querySelector(".form");
-const item = document.getElementById("todo");
-
-itemForm.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const newItem = item.value;
+  // A. Select the Input Todo Value
+  const inputVal = todoInput.value;
 
-  const newListItem = document.createElement("li");
-  newListItem.appendChild(document.createTextNode(newItem));
-  todoList.appendChild(newListItem);
-  item.value = "";
+  // B. Create a new ListItem with the text Input Value
+  const listItem = document.createElement("li");
+  listItem.innerText = inputVal;
+
+  console.log(listItem);
+  // C. Add Listitem to the List as Child
+  list.appendChild(listItem);
+
+  // D. Clear the Input Feild
+  todoInput.value = "";
+
+  // E. Add Eventlistener on Listitem to remove the item when clicked
+  listItem.addEventListener("click", () => {
+    listItem.remove();
+  });
 });
