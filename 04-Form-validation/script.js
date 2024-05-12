3eb9d480637aef3440e27d3d2ddb68ed112f64dd
saurav-verma-01 <sauravkumarverma22@gmail.com>
2024-05-12 23:10:04 +0530

    1st step done

--- 04-Form-validation/script.js
+++ 04-Form-validation/script.js
@@ -1,14 +1,52 @@
 // TODO: Select all elements needed
 //    Use the HTML to figure out what classes/ids will work best for selecting each element
 
+// Selecting all Elements Needed
+const formEl = document.getElementById("form");
+const userNameEl = document.getElementById("username");
+const passwordEl = document.getElementById("password");
+const passwordConfirmationEl = document.getElementById("password-confirmation");
+const termsEl = document.getElementById("terms");
+
+const errorDiv = document.querySelector(".errors");
+const errorList = document.querySelector(".errors-list");
 // TODO: Create an event listener for when the form is submitted and do the following inside of it.
-//    TODO: Create an array to store all error messages and clear any old error messages
-//    TODO: Define the following validation checks with appropriate error messages
-//      1. Ensure the username is at least 6 characters long
-//      2. Ensure the password is at least 10 characters long
-//      3. Ensure the password and confirmation password match
-//      4. Ensure the terms checkbox is checked
-//    TODO: If there are any errors then prevent the form from submitting and show the error messages
+
+let errorArray = [];
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Form Submitted");
+  //    TODO: Create an array to store all error messages and clear any old error messages
+  errorArray.splice(0, errorArray.length);
+  //    TODO: Define the following validation checks with appropriate error messages
+
+  //      1. Ensure the username is at least 6 characters long
+  if (userNameEl.value.length < 6) {
+    errorArray.push("Username must be atleast 6 characters long.");
+  }
+  //      2. Ensure the password is at least 10 characters long
+
+  if (passwordEl.value.length < 10) {
+    errorArray.push("Password must be atleast 10 characters long.");
+  }
+  //      3. Ensure the password and confirmation password match
+  if (passwordConfirmationEl.value !== passwordEl.value) {
+    errorArray.push("Password and Confirm Password should Match.");
+  }
+  //      4. Ensure the terms checkbox is checked
+  if (!termsEl.checked) {
+    errorArray.push("You must accept the terms.");
+  }
+  //    TODO: If there are any errors then prevent the form from submitting and show the error messages
+
+  showErrors(errorArray);
+  if (errorArray.length > 0) {
+    errorDiv.classList.add("show");
+  } else {
+    errorDiv.classList.remove("show");
+  }
+});
 
 // TODO: Define this function
 function clearErrors() {
@@ -22,6 +60,11 @@ function clearErrors() {
 // TODO: Define this function
 function showErrors(errorMessages) {
   // Add each error to the error-list element
-  // Make sure to use an li as the element for each error
-  // Also, make sure you add the show class to the errors container
+  errorMessages.forEach((error) => {
+    // Make sure to use an li as the element for each error
+    const errorListItem = document.createElement("li");
+    errorListItem.innerText = error;
+    errorDiv.appendChild(errorListItem);
+    // Also, make sure you add the show class to the errors container
+  });
 }
